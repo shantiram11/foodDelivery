4468725842b4cdef00a1510aa2d138614710c633
Pawal Karki <pawal_karki@icloud.com>
2025-07-20 21:25:41 +0545

    fix(01): Fixed the Controller and its route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\FrontController;
+use App\Http\Controllers\Front\FrontController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[FrontController::class,'index'])->name('home');
